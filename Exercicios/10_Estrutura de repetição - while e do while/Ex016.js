5e4fda40d60af4eac4b9445f1588cdeb11af7651
Julio Cesar Baccin <juliobassin@hotmail.com>
2022-05-25 23:57:19 -0300

    confirmar

---
+++ Exercicios/10_Estrutura de repetição - while e do while/Ex016.js
@@ -0,0 +1,24 @@
+// Aqui temos uma estrutura do while, primeiro executa o bloco e depois ele testa
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}
+while (c <= 3) {
+
+}
+
+// Aqui temos uma estrutura while, testa primeiro e executa depois
+var c = 1
+while(c <= 3){
+    console.log(`passo ${c}`)
+    c++
+}
+
+// Maneira mais manual de fazer a estrutura de repetição, praticamente obsoleta.
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
---
+++ Exercicios/11_Estrutura de repetição - for/Ex017.js
@@ -0,0 +1,9 @@
+console.log('Vai começar...')
+for (c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('Fim!')
+
+for (var c = 1; c < 5; c++) {
+    console.log(c)
+}
